206ef9d0b49296178af8e385a310ab4598c0ab19
nicoubina <gorodriguez@alumno.huergo.edu.ar>
2025-08-20 11:47:40 -0300

    cambios styles.css

--- djangoTutorial/PortfolfioDjango/mysite/blog/models.py
+++ djangoTutorial/PortfolfioDjango/mysite/blog/models.py
@@ -16,6 +16,10 @@ class Post(models.Model):
     
     def publish(self):
         self.published_date = timezone.now()
+    
+    def approved_comments(self):
+      return self.comments.filter(approved_comment=True)
+
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
@@ -31,5 +35,4 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
 
-def approved_comments(self):
-    return self.comments.filter(approved_comment=True)
+ 
--- djangoTutorial/PortfolfioDjango/mysite/mysite/urls.py
+++ djangoTutorial/PortfolfioDjango/mysite/mysite/urls.py
@@ -23,11 +23,11 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 if not settings.TESTING:
     urlpatterns = [
+
         path("blog/", include("blog.urls")),
         
         path("", include("portfolio.urls")),
         
         path("admin/", admin.site.urls),
-    
     
     ] + debug_toolbar_urls()
